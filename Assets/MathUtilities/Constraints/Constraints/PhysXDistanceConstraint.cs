697670b7b19a458c535c615c359eed7d0cd58b96
AnnFyz <64531297+AnnFyz@users.noreply.github.com>
2025-04-28 16:22:43 +0200

    add MathUtilities package

---
+++ Assets/MathUtilities/Constraints/Constraints/PhysXDistanceConstraint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+public class VelocityConstraint : MonoBehaviour {
+  Rigidbody body;
+  public Rigidbody otherBody;
+  public float Dist = 1f;
+  void Start() {
+    body = GetComponent<Rigidbody>();
+  }
+  void FixedUpdate() {
+    ConstrainRigidbodies(body, otherBody, Dist * Dist);
+  }
+  void ConstrainRigidbodies(Rigidbody body, Rigidbody otherBody, float sqrDist) {
+    Vector3 offset = (otherBody.position + otherBody.linearVelocity * Time.fixedDeltaTime) - (body.position + body.linearVelocity * Time.fixedDeltaTime);
+    if ((offset).sqrMagnitude > sqrDist) {
+      offset *= sqrDist / (offset.sqrMagnitude + sqrDist) - 0.5f;
+      body.linearVelocity -= offset / Time.fixedDeltaTime;
+      otherBody.linearVelocity += offset / Time.fixedDeltaTime;
+    }
+  }
+}
