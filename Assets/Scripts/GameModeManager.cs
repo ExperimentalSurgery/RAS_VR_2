34267dd3891938309fdf7f27776fdb66de3b8451
AnnFyz <64531297+AnnFyz@users.noreply.github.com>
2025-05-21 14:54:46 +0200

    passthrough mode

--- Assets/Scripts/GameModeManager.cs
+++ Assets/Scripts/GameModeManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class GameModeManager : MonoBehaviour
 {
+    [Header("OVRPassthroughLayer")]
+    [SerializeField] private OVRPassthroughLayer oVRPassthroughLayer;
+    [SerializeField] float timeForToggleEnabling = 1f;
+    [SerializeField] bool canToggle = true;
+
     [Header("Material Settings")]
     [SerializeField] Material glowMaterial;
     [SerializeField] Material defaultMat;
@@ -19,17 +24,48 @@ public class GameModeManager : MonoBehaviour
     [SerializeField] HapticPlugin hapticPluginR;
     [SerializeField] HapticPlugin hapticPluginL;
 
+    [Header("Cameras")]
+    [SerializeField] Camera[] cameras;
+
+    private void Awake()
+    {
+        oVRPassthroughLayer.passthroughLayerResumed.AddListener(OnPassthroughLayerResumed);
+        // 1) We enable the passthrough layer to kick off its initialization process
+        oVRPassthroughLayer.enabled = true;
+    }
+
+
+
+    private void OnDestroy()
+    {
+        oVRPassthroughLayer.passthroughLayerResumed.RemoveListener(OnPassthroughLayerResumed);
+    }
+
+    // 2) OnPassthroughLayerResumed is called once the layer is fully initialized and passthrough is visible
+    private void OnPassthroughLayerResumed(OVRPassthroughLayer passthroughLayer)
+    {
+        // 3) Do something here after the passthrough layer has resumed
+    }
+
     private void Start()
     {
         isVirtualReality = false;
-       // rightStylusCollider.enabled = false;
-      //  leftStylusCollider.enabled = false;
+        // rightStylusCollider.enabled = false;
+        //  leftStylusCollider.enabled = false;
+        cameras = FindObjectsByType<Camera>(FindObjectsInactive.Include,FindObjectsSortMode.None);
     }
     public void ToggleGameMode(bool isRightStylus)
     {
+        StartCoroutine(StatTimerForToggling());
+        if (!canToggle)
+        {
+            Debug.Log("Cannot toggle game mode yet.");
+            return;
+        }
         isVirtualReality = !isVirtualReality;
         if (isVirtualReality)
         {
+            oVRPassthroughLayer.enabled = false;
             rightStylusCollider.enabled = true;
             leftStylusCollider.enabled = true;
             SwapMaterial(true, isRightStylus);
@@ -38,6 +74,7 @@ public class GameModeManager : MonoBehaviour
         }
         else
         {
+            oVRPassthroughLayer.enabled = true;
             rightStylusCollider.enabled = false;
             leftStylusCollider.enabled = false;
             SwapMaterial(false, isRightStylus);
@@ -65,5 +102,19 @@ public class GameModeManager : MonoBehaviour
         yield return new WaitForSeconds(.5f);
         hapticPluginR.DisableVibration();
         hapticPluginL.DisableVibration();
+    }
+
+    IEnumerator StatTimerForToggling()
+    {
+        canToggle = false;
+        while (timeForToggleEnabling > 0)
+        {
+            Debug.Log($"Timer running: {timeForToggleEnabling} seconds remaining");
+            yield return new WaitForSeconds(1f);
+            timeForToggleEnabling -= 1f;
+        }
+        canToggle = true;
+        Debug.Log("Timer finished!");
+        // Add any logic here that should run after the timer ends
     }
 }
