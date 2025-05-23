7a1e0604b6217b5dee10f59e5516cacd0cfa18ff
AnnFyz <64531297+AnnFyz@users.noreply.github.com>
2025-05-23 17:22:29 +0200

    added kabsch plugin

---
+++ Assets/RAS_Callibration/3rd Party/KabschCalibrationUnity/Editor/CalibrationManagerEditor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CalibrationManager))]
+public class CalibrationManagerEditor : Editor
+{
+    // OnInspector GUI
+    public override void OnInspectorGUI()
+    {
+        base.DrawDefaultInspector();
+        CalibrationManager calibrationManagerTarget = (CalibrationManager)target;
+
+        if (Application.isPlaying)
+        {              
+            calibrationManagerTarget.ChoiceIndex = EditorGUILayout.Popup(calibrationManagerTarget.ChoiceIndex, calibrationManagerTarget.AlignObjectChoices);
+            calibrationManagerTarget.ObjectToCalibrate = calibrationManagerTarget.AlignObjectsInScene[calibrationManagerTarget.ChoiceIndex];
+        }
+
+        GUILayout.Space(20);
+
+        if (GUILayout.Button("Calibrate"))
+        {
+            calibrationManagerTarget.Calibrate();
+        }
+
+        GUILayout.Space(20);
+
+        if (GUILayout.Button("Reset last target points")) 
+        {
+            calibrationManagerTarget.ResetLastTargetPoint();
+            calibrationManagerTarget.ChangeColorOfPointer();
+        }
+        
+        if (GUILayout.Button("Reset all target points")) 
+        {
+            calibrationManagerTarget.ResetTargetPoints();
+            calibrationManagerTarget.ChangeColorOfPointer();
+        }
+
+        if (GUILayout.Button("Load")) 
+        {
+            calibrationManagerTarget.LoadCalibrationFromFile();
+        }
+
+        if (GUILayout.Button("Save")) 
+        {
+            calibrationManagerTarget.SaveCalibrationToFile();
+        }
+    }
+}
