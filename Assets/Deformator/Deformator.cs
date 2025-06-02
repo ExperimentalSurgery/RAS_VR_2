4f5a2e80e2b5ce29be5770af7b8c2edb2c99f65f
AnnFyz <64531297+AnnFyz@users.noreply.github.com>
2025-06-02 12:28:30 +0200

    add

--- Assets/Deformator/Deformator.cs
+++ Assets/Deformator/Deformator.cs
@@ -2,20 +2,29 @@ using UnityEngine;
 
 public class Deformator : MonoBehaviour
 {
-    [SerializeField]
-    Transform pointerTip;
+    [Header("Pointer 1")]
 
     [SerializeField]
-    Transform pointerTail;
+    Transform pointer1Tip;
 
     [SerializeField]
-    Transform pointerDirZ;
+    Transform pointer1Tail;
 
     [SerializeField]
-    Transform surfacePosition;
+    Transform pointer1DirZ;
+
+    [Header("Pointer 2")]
 
     [SerializeField]
-    Transform depthPosition;
+    Transform pointer2Tip;
+
+    [SerializeField]
+    Transform pointer2Tail;
+
+    [SerializeField]
+    Transform pointer2DirZ;
+
+    [Header("Deformed Object Settings")]
 
     [SerializeField]
     MeshRenderer deformMeshRenderer;
@@ -32,19 +41,43 @@ public class Deformator : MonoBehaviour
     [SerializeField]
     float deformRadius;
 
-    int isInObject;
+    [Header("Debug Visualisation (optional)")]
 
+    [SerializeField]
+    bool enableDebugVisualisations;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    GameObject depthVizPrefab;
+
+    [SerializeField]
+    GameObject surfaceVizPrefab;
+
+    GameObject surfaceViz1;
+    GameObject surfaceViz2;
+    GameObject depthViz1;
+    GameObject depthViz2;
+
+    private void Start()
     {
+        
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        CalculateDeformation(pointer1Tip, pointer1Tail, pointer1DirZ, 1);
+        CalculateDeformation(pointer2Tip, pointer2Tail, pointer2DirZ, 2);
+    }
+
+    void CalculateDeformation(Transform pointerTip, Transform pointerTail, Transform pointerDirZ, int pointerIndex )
+    {
         RaycastHit rayInfoTip;
         RaycastHit rayInfoTail;
+        int isInObject;
+
+        Vector3 surfacePos;
+        Vector3 depthPos;
 
         bool hitTip = Physics.Raycast(new Ray(pointerTip.position, pointerDirZ.forward), out rayInfoTip, 3f, deformLayer);
         bool hitTail = Physics.Raycast(new Ray(pointerTail.position, pointerDirZ.forward), out rayInfoTail, 3f, deformLayer);
@@ -53,37 +86,105 @@ public class Deformator : MonoBehaviour
         if (!hitTip && hitTail)
         {
             isInObject = 1;
-            surfacePosition.position = rayInfoTail.point;
-        }
-
-        else 
-        {
-            isInObject = 0;
-            surfacePosition.position = pointerTip.position;
-        }
-
-        //Limit the max deform depth
-        if (Vector3.Distance(surfacePosition.position, pointerTip.position) > maxDepth)
-        {
-            Vector3 dir = (pointerTip.position - surfacePosition.position).normalized;
-            depthPosition.position = surfacePosition.position + (dir * maxDepth);
+            surfacePos = rayInfoTail.point;
         }
 
         else
         {
-            depthPosition.position = pointerTip.position;
+            isInObject = 0;
+            surfacePos= pointerTip.position;
+        }
+
+        //Limit the max deform depth
+        if (Vector3.Distance(surfacePos, pointerTip.position) > maxDepth)
+        {
+            Vector3 dir = (pointerTip.position - surfacePos).normalized;
+            depthPos = surfacePos + (dir * maxDepth);
+        }
+
+        else
+        {
+            depthPos = pointerTip.position;
         }
 
         //Get the deformation depth
-        float deformDepth = Vector3.Distance(surfacePosition.position, depthPosition.position);
+        float deformDepth = Vector3.Distance(surfacePos, depthPos);
 
 
-        //Set Deformation Shader values
-        deformMeshRenderer.sharedMaterial.SetVector("_SurfaceContactPosition", surfacePosition.position);
-        deformMeshRenderer.sharedMaterial.SetVector("_DeformationPosition", depthPosition.position);
-        deformMeshRenderer.sharedMaterial.SetFloat("_DeformDepth", deformDepth);
+        //Set the Pointer-Dependent Deformation Shader values
+        if(pointerIndex == 1)
+        {
+            deformMeshRenderer.sharedMaterial.SetVector("_SurfaceContactPosition1", surfacePos);
+            deformMeshRenderer.sharedMaterial.SetVector("_DeformationPosition1", depthPos);
+            deformMeshRenderer.sharedMaterial.SetFloat("_IsBeingDeformed1", isInObject);
+            deformMeshRenderer.sharedMaterial.SetFloat("_DeformDepth1", deformDepth);
+
+        }
+
+        if (pointerIndex == 2)
+        {
+            deformMeshRenderer.sharedMaterial.SetVector("_SurfaceContactPosition2", surfacePos);
+            deformMeshRenderer.sharedMaterial.SetVector("_DeformationPosition2", depthPos);
+            deformMeshRenderer.sharedMaterial.SetFloat("_IsBeingDeformed2", isInObject);
+            deformMeshRenderer.sharedMaterial.SetFloat("_DeformDepth2", deformDepth);
+
+        }
+
+        //Set the commen Deformation Shader values
         deformMeshRenderer.sharedMaterial.SetFloat("_MaxDepth", maxDepth);
         deformMeshRenderer.sharedMaterial.SetFloat("_DeformRadius", deformRadius);
-        deformMeshRenderer.sharedMaterial.SetFloat("_IsBeingDeformed", isInObject);
+
+        //Debug visualisations
+        ShowDebugVisualisation(surfacePos, depthPos, pointerIndex);
+
+    }
+
+    void ShowDebugVisualisation(Vector3 surfaceVizPos, Vector3 depthVizPos, int pointerIndex)
+    {
+        if (enableDebugVisualisations)
+        {
+            if (surfaceViz1 == null)
+                surfaceViz1 = Instantiate(surfaceVizPrefab);
+            if (surfaceViz2 == null)
+                surfaceViz2 = Instantiate(surfaceVizPrefab);
+            if (depthViz1 == null)
+                depthViz1 = Instantiate(depthVizPrefab);
+            if (depthViz2 == null)
+                depthViz2 = Instantiate(depthVizPrefab);
+
+            surfaceViz1.transform.parent = pointer1Tip;
+            surfaceViz2.transform.parent = pointer2Tip;
+            depthViz1.transform.parent = pointer1Tip;
+            depthViz2.transform.parent = pointer2Tip;
+        }
+
+        else
+        {
+            if(surfaceViz1 != null)
+                Destroy(surfaceViz1);
+            if (surfaceViz2 != null)
+                Destroy(surfaceViz2);
+            if (depthViz1 != null)
+                Destroy(depthViz1);
+            if (depthViz2 != null)
+                Destroy(depthViz2);
+        }
+
+        if(pointerIndex == 1)
+        {
+            if (surfaceViz1 != null)
+                surfaceViz1.transform.position = surfaceVizPos;
+            if (depthViz1 != null)
+                depthViz1.transform.position = depthVizPos;
+        }
+
+        if(pointerIndex == 2)
+        {
+            if (surfaceViz2 != null)
+                surfaceViz2.transform.position = surfaceVizPos;
+            if (depthViz2 != null)
+                depthViz2.transform.position = depthVizPos;
+        }
+
     }
 }
