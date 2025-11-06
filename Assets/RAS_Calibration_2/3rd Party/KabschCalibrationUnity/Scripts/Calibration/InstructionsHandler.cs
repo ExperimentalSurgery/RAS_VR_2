fb1665be7f2cbd6ad7d5d29a3a6252258a281f8a
AnnFyz <64531297+AnnFyz@users.noreply.github.com>
2025-11-06 15:29:35 +0100

    add

--- Assets/RAS_Calibration_2/3rd Party/KabschCalibrationUnity/Scripts/Calibration/InstructionsHandler.cs
+++ Assets/RAS_Calibration_2/3rd Party/KabschCalibrationUnity/Scripts/Calibration/InstructionsHandler.cs
@@ -10,7 +10,8 @@ public class InstructionsHandler : MonoBehaviour
     [SerializeField]
     string[] instructions;
     CalibrationManager calibrationManager;
-
+    [SerializeField]
+    SequenceHandler sequenceHandler;
     private void Awake()
     {
         calibrationManager = GetComponent<CalibrationManager>();
@@ -18,23 +19,42 @@ public class InstructionsHandler : MonoBehaviour
 
     private void OnEnable()
     {
+        sequenceHandler.onFinishedReading += DisplayFirstInstruction;
         calibrationManager.OnCalibrationComplete += SelectInstruction;
+    }
+
+    private void OnDisable()
+    {
+        sequenceHandler.onFinishedReading -= DisplayFirstInstruction;
+        calibrationManager.OnCalibrationComplete -= SelectInstruction;
+    }
+
+
+    private void DisplayFirstInstruction() // for right controller
+    {
+        uiIndex = calibrationManager.CalibrationPointIndex;
+        uiInstructionsText.text = instructions[0];
+    }
+
+    private void DisplayInstrictionForStyluses()
+    {
+
     }
 
 
     void SelectInstruction()
     {
         uiIndex = calibrationManager.CalibrationPointIndex;
-        indexText.text = "Next socket: " + (uiIndex + 1).ToString();
+        indexText.text = "Next socket: " + (uiIndex +2);
         if (calibrationManager == null) return;
 
         if (calibrationManager.ObjectToCalibrate == calibrationManager.AlignObjectsInScene[0])
         {
             switch (uiIndex)
             {
-                case 0:
-                    uiInstructionsText.text = instructions[0];
-                    break;
+                //case 0:
+                //    uiInstructionsText.text = instructions[0];
+                //    break;
                 default:
                     uiInstructionsText.text = instructions[3];
                     break;
@@ -45,9 +65,9 @@ public class InstructionsHandler : MonoBehaviour
         {
             switch (uiIndex)
             {
-                case 0:
-                    uiInstructionsText.text = instructions[1];
-                    break;
+                //case 0:
+                //    uiInstructionsText.text = instructions[1];
+                //    break;
                 default:
                     uiInstructionsText.text = instructions[3];
                     break;
@@ -58,9 +78,9 @@ public class InstructionsHandler : MonoBehaviour
         {
             switch (uiIndex)
             {
-                case 0:
-                    uiInstructionsText.text = instructions[1];
-                    break;
+                //case 0:
+                //    uiInstructionsText.text = instructions[1];
+                //    break;
                 default:
                     uiInstructionsText.text = instructions[3];
                     break;
