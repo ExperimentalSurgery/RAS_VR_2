a3da13b14a80232e289cfa51e6ab23f682c757e8
Anna <64531297+AnnFyz@users.noreply.github.com>
2025-11-20 09:06:36 +0100

    added instructions for setting 1

--- Assets/Scripts/StepTextHandler.cs
+++ Assets/Scripts/StepTextHandler.cs
@@ -4,12 +4,14 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Reflection.Emit;
 
 public class StepTextHandler : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI textLabelObject;
     [SerializeField] TextMeshProUGUI textBodyObject;
     [SerializeField] string[] stepLabelTexts;
+    [SerializeField] GameObject[] stepArrows;
     //[SerializeField] string[] stepBodyTexts;
     [SerializeField] TMP_Text nextButton;
     int m_CurrentStepIndex = 0;
@@ -17,6 +19,10 @@ public class StepTextHandler : MonoBehaviour
 
     private void Start()
     {
+        foreach (var arrow in stepArrows)
+        {
+            arrow.SetActive(false);
+        }
         NextSetting();
     }
 
@@ -39,6 +45,7 @@ public class StepTextHandler : MonoBehaviour
         textLabelObject.text = stepLabelTexts[m_CurrentStepIndex];
         // body
         //textBodyObject.text = stepBodyTexts[m_CurrentStepIndex];
+        SelectArrows();
 
         m_CurrentStepIndex = (m_CurrentStepIndex + 1) % stepLabelTexts.Length;
     }
@@ -50,6 +57,66 @@ public class StepTextHandler : MonoBehaviour
         int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
         SceneManager.LoadScene(nextSceneIndex);
         Debug.Log("Load next scene");
+    }
+
+    void SelectArrows()
+    {
+        switch (m_CurrentStepIndex)
+        {
+            case 0:
+                foreach (var arrow in stepArrows)
+                {
+                    arrow.SetActive(false);
+                }
+                stepArrows[0].SetActive(true);
+                break;
+            case 1:
+                foreach (var arrow in stepArrows)
+                {
+                    arrow.SetActive(false);
+                }
+                stepArrows[1].SetActive(true);
+                break;
+            case 2:
+                foreach (var arrow in stepArrows)
+                {
+                    arrow.SetActive(false);
+                }
+                stepArrows[2].SetActive(true);
+                break;
+            case 3:
+                foreach (var arrow in stepArrows)
+                {
+                    arrow.SetActive(false);
+                }
+                stepArrows[3].SetActive(true);
+                stepArrows[4].SetActive(true);
+                break;
+            case 4:
+                foreach (var arrow in stepArrows)
+                {
+                    arrow.SetActive(false);
+                }
+                stepArrows[5].SetActive(true);
+                break;
+            case 5:
+                foreach (var arrow in stepArrows)
+                {
+                    arrow.SetActive(false);
+                }
+                stepArrows[6].SetActive(true);
+                stepArrows[7].SetActive(true);
+                break;
+            case 6:
+                foreach (var arrow in stepArrows)
+                {
+                    arrow.SetActive(false);
+                }
+                stepArrows[8].SetActive(true);
+                break;
+            default:
+                break;
+        }
     }
 }
 
